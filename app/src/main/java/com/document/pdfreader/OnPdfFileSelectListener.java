c192f8525d0cdc7e64920da3a61ea56985a77f65
yash <yashkumawat552@gmail.com>
2022-08-15 21:34:35 +0530

    PDF Reader Ready

---
+++ app/src/main/java/com/document/pdfreader/OnPdfFileSelectListener.java
@@ -0,0 +1,7 @@
+package com.document.pdfreader;
+
+import java.io.File;
+
+public interface OnPdfFileSelectListener {
+    void onPdfSelected(File file);
+}
---
+++ app/src/test/java/com/document/pdfreader/PdfAdapterTest.java
@@ -0,0 +1,7 @@
+package com.document.pdfreader;
+
+import junit.framework.TestCase;
+
+public class PdfAdapterTest extends TestCase {
+
+}
